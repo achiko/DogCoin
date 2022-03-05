cb1e567d3e6489d2fc89ba8025157c7e2d183e44
achiko <achiko@gmail.com>
2022-03-05 23:40:06 +0400

    Added Lesson 1 & 2 assignemts

--- contracts/DocgCoin.sol
+++ contracts/DocgCoin.sol
@@ -7,6 +7,9 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
+import "hardhat/console.sol";
+
+
 contract DogCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     uint public test1;
     uint public test2;
@@ -21,12 +24,9 @@ contract DogCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, O
 
         test1 = 1;
         test2 = 2;
-
-        // Mint 1000 tokens for admin
-        _mint(msg.sender, 1000 * 10 ** decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public virtual onlyOwner {
         _mint(to, amount);
     }
 
@@ -39,12 +39,60 @@ contract DogCoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, O
 
 contract DogCoinV2 is DogCoin {
     uint public test3;
+    address[] public accounts;
 
     function version() public pure returns (string memory) {
         return "V2";
     }
 
-    // function mint()
+    function mint(address to, uint256 amount) public override onlyOwner {
+        _mint(to, amount);
+        _addAcountTolist(to);
+    }
+
+    // Internal Function Add account into list 
+    function _addAcountTolist(address _account) internal returns(bool) {
+        for (uint i = 0; i < accounts.length; i++) {
+            if(accounts[i] == _account) {
+                return false;
+            }
+        }
+        accounts.push(_account);
+        return true;
+    }
+
+    // Remove address from list 
+    function _removeAccountFromList(address _account) internal {
+        uint index = 0;
+        for (uint i = 0; i < accounts.length; i++) {
+            if(accounts[i] == _account) {
+                index = i;
+            }
+        }
+        // move the last element into the place
+        accounts[index] = accounts[accounts.length - 1];
+        // delete
+        accounts.pop();
+    }
+
+
+    // address to, uint256 amount
+    function transfer(address to, uint256 amount) public override virtual returns (bool) {
+        bool success = super.transfer(to, amount); 
+        require(success, "ERR");   
+        
+        // Check balance of sender 
+        uint256 senderBalance = balanceOf(msg.sender);
+        if(senderBalance == 0) {
+            _removeAccountFromList(msg.sender);
+        }
+        _addAcountTolist(to);
+        return success;
+    }
+
+    function getAccountList() public view returns(address[] memory) {
+        return accounts;
+    }
 
     function updateTest3(uint _value) public {
         test3 = _value;
--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,8 @@
 import { ethers, upgrades } from "hardhat";
 import { Signer } from "ethers";
 import { assert } from "chai";
+import { formatUnits, parseUnits } from "ethers/lib/utils";
+import { utils, providers } from "ethers";
 
 import {
   DogCoin,
@@ -8,7 +10,6 @@ import {
   DogCoinV2,
   DogCoinV2__factory,
 } from "../typechain";
-import { contains } from "underscore";
 
 describe("DogCoin", function () {
   let accounts: Signer[];
@@ -31,31 +32,89 @@ describe("DogCoin", function () {
     console.log("Version after upgrade : ", await dogCoin_V2.version());
   });
 
-  it("#2 Sould test smartcontract storage varriables ", async function () {
+  it("#2 Sould test smartcontract storage varriables after V2 upgrade ", async function () {
     let dogCoinContract = DogCoinV2__factory.connect(
       contractAddress,
       accounts[0]
     );
 
-    assert.isOk(
+    assert.isTrue(
       (await dogCoinContract.test1()).eq(1),
       "WRONG TEST1 STORAGE VARIABLE"
     );
-    assert.isOk(
+    assert.isTrue(
       (await dogCoinContract.test2()).eq(2),
       "WRONG TEST2 STORAGE VARIABLE IS"
     );
 
     await dogCoinContract.updateTest3(3);
 
-    assert.isOk(
+    assert.isTrue(
       (await dogCoinContract.test3()).eq(3),
       "WRONG TEST3 STORAGE VARIABLE"
     );
   });
 
-  it("#3 Airdrop Tokens for accounts  ", async function () {
-    let [admin, accountA, accountB, accountC] = accounts;
+  it("#3 Mint & Transfer Tokens", async function () {
+    /**
+     * Scenarios :
+     * 1. Get 3 Accounts Account1, Account3 from signers
+     * 2. Mint 100 Tokens for Account1
+     * 3. Mint 150 Tokens For Account2
+     * 4. Transfer All tokens from Account2 to Account3
+     * 5. Mint 200 Tokens For Account1
+     */
+
+    let [admin, account1, account2, account3] = accounts;
     const contract = DogCoinV2__factory.connect(contractAddress, admin);
+
+    const account1Addr = await account1.getAddress();
+    const account2Addr = await account2.getAddress();
+    const account3Addr = await account3.getAddress();
+
+    const { mint, balanceOf } = contract;
+    await mint(account1Addr, parseUnits("100", 18));
+    assert.equal(
+      (await contract.getAccountList()).length,
+      1,
+      "WRONG ACCOUNTS LIST"
+    );
+    await mint(account2Addr, parseUnits("150", 18));
+
+    assert.equal(
+      (await contract.getAccountList()).length,
+      2,
+      "WRONG ACCOUNTS LIST"
+    );
+
+    assert.isTrue((await balanceOf(account1Addr)).eq(parseUnits("100", 18)));
+    assert.isTrue((await balanceOf(account2Addr)).eq(parseUnits("150", 18)));
+
+    await contract
+      .connect(account2)
+      .transfer(account3Addr, parseUnits("150", 18));
+
+    assert.isTrue((await balanceOf(account2Addr)).eq(0));
+    assert.isTrue((await balanceOf(account3Addr)).eq(parseUnits("150", 18)));
+
+    assert.equal(
+      (await contract.getAccountList()).length,
+      2,
+      "WRONG ACCOUNTS LIST"
+    );
+
+    await mint(await account1.getAddress(), parseUnits("200", 18));
+
+    assert.equal(
+      (await contract.getAccountList()).length,
+      2,
+      "WRONG ACCOUNTS LIST"
+    );
+
+    assert.equal(
+      (await contract.getAccountList()).indexOf(account2Addr),
+      -1,
+      "ACCOUNTS LIST"
+    );
   });
 });
